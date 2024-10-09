d95bbfc5b69fc790a7ec60fe2152f987ddbbefea
HP <HP@DESKTOP-CR3PTC8>
2024-10-09 16:37:04 +0530

    My First Commit

---
+++ src/main/java/com/javaprograms1/p1/TestCode.java
@@ -0,0 +1,11 @@
+package com.javaprograms1.p1;
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
